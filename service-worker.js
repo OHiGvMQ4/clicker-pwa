d70718ff75bbdde9e9e308e303442e0b7a1c39d4
OHiGvMQ4 <Nguyenduykhanhkt10@gmail.com>
2025-04-24 21:31:33 +0700

    =)
    test

---
+++ service-worker.js
@@ -0,0 +1,22 @@
+const CACHE_NAME = 'clicker-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/icon-192.png',
+  '/icon-512.png',
+  'https://www.myinstants.com/media/sounds/click.mp3',
+  'https://www.myinstants.com/media/sounds/cash-register.mp3'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
+});
